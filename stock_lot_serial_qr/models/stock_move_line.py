180ab084ce7373845a87bc1d90b022fac0bfde0a
Tim <tl@quartile.co>
2019-06-24 15:13:03 +0800

    [ADD] stock_lot_serial_qr (#7)

---
+++ stock_lot_serial_qr/models/stock_move_line.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import base64
+from odoo import api, fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    # this will check if new lot will appear on incoming shipment
+    # then it will create serial numbers for newly created lot
+    @api.multi
+    def write(self, vals):
+        result = super(StockMoveLine, self).write(vals)
+        if 'lot_id' in vals:
+            for move_line in self.filtered(
+                    lambda x: x.picking_id.picking_type_code == 'incoming' and 
+                    not x.move_id.origin_returned_move_id):
+                if move_line.qty_done and move_line.lot_id:
+                    move_line.lot_id._create_stock_serial(move_line.qty_done)
+        return result
