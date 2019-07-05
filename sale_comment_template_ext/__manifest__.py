ca34bc7bb59e6873f65a082c40924502d6fb07f2
Yoshi Tashiro <tashiro@quartile.co>
2019-07-05 13:22:24 +0900

    add sale_comment_template (#13)

---
+++ sale_comment_template_ext/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Comments Ext",
+    "summary": "Passes note field values to picking",
+    "version": "12.0.1.0.0",
+    "category": "Sale",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "sale_comment_template",
+        "stock_picking_comment_template",
+    ],
+}
