f94696215fb4c16595ee459f46410bf967065d02
Yoshi Tashiro <tashiro@quartile.co>
2019-07-06 17:23:13 +0800

    add product_ext_rpl

---
+++ product_ext_rpl/models/product_template.py
@@ -0,0 +1,30 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    label_ref = fields.Char(
+        "Label Reference",
+        compute='_compute_label_ref',
+        inverse='_set_label_ref',
+        store=True,
+        help="The value set here will be used as 'REF' in product label "
+            "print.",
+    )
+
+    @api.depends('product_variant_ids', 'product_variant_ids.label_ref')
+    def _compute_label_ref(self):
+        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+        for template in unique_variants:
+            template.label_ref = template.product_variant_ids.label_ref
+        for template in (self - unique_variants):
+            template.label_ref = ''
+    
+    @api.one
+    def _set_label_ref(self):
+        if len(self.product_variant_ids) == 1:
+            self.product_variant_ids.label_ref = self.label_ref
