61fb530095b62070ef240a96456d206027d6c788
tslai <tl@quartile.co>
2020-03-17 11:35:28 +0800

    [ADD] black, isort

--- stock_quant_list_view/__manifest__.py
+++ stock_quant_list_view/__manifest__.py
@@ -1,18 +1,14 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock Quant List View',
-    'version': '12.0.1.0.0',
-    'author': 'Quartile Limited',
-    'website': 'https://www.quartile.co',
-    'category': 'Stock',
-    'license': "AGPL-3",
-    'description': "",
-    'depends': [
-        'stock',
-    ],
-    'data': [
-        'views/stock_quant_views.xml',
-    ],
-    'installable': True,
+    "name": "Stock Quant List View",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "description": "",
+    "depends": ["stock",],
+    "data": ["views/stock_quant_views.xml",],
+    "installable": True,
 }
