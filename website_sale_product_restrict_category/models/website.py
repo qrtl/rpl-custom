cc8b7c9679985ca0e0084547b1e85249eed0953a
YogeshMahera-SerpentCS <yogesh.mahera.serpentcs@gmail.com>
2020-04-17 14:16:10 +0530

    [FIX] Fix Pylint

--- website_sale_product_restrict_category/models/website.py
+++ website_sale_product_restrict_category/models/website.py
@@ -10,10 +10,12 @@ class Website(models.Model):
     @api.multi
     def sale_product_domain(self):
         domain = super(Website, self).sale_product_domain()
-        if self.env.user.has_group("base.group_public") or self.env.user.has_group("base.group_portal"
+        if self.env.user.has_group("base.group_public") or self.env.user.has_group(
+            "base.group_portal"
         ):
-            public_category_ids = \
+            public_category_ids = (
                 self.env.user.commercial_partner_id.public_category_ids
+            )
             domain += [
                 "|",
                 ("is_public", "=", True),
