ec8877fde491ec5fffd7299500b0b13ff3b7405a
Yoshi Tashiro <tashiro@quartile.co>
2020-04-18 17:50:17 +0800

    [IMP] flake8

--- website_sale_restrict_category/models/public_category.py
+++ website_sale_restrict_category/models/public_category.py
@@ -1,7 +1,7 @@
 ﻿# Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductPublicCategory(models.Model):
