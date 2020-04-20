d8127835a7643443e6de850fad38ff2271ec8bb6
Yoshi Tashiro <tashiro@quartile.co>
2020-04-20 18:26:40 +0900

    [ADD] website_sale_restrict_category (#21)

---
+++ website_sale_restrict_category/models/website.py
@@ -0,0 +1,28 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, models
+
+
+class Website(models.Model):
+    _inherit = "website"
+
+    @api.multi
+    def sale_product_domain(self):
+        """ Even if a product belongs to a restricted category, the product
+            should still show if it also belongs to a non-restricted category.
+        """
+        res = super(Website, self).sale_product_domain()
+        user = self.env.user
+        if user.has_group("base.group_public") or user.has_group("base.group_portal"):
+            public_categ = self.env["product.public.category"]
+            restricted_categs = public_categ.browse()
+            for categ in user.commercial_partner_id.public_category_ids:
+                restricted_categs += categ.get_offsprings()
+            allowed_categs = public_categ.search([]) - restricted_categs
+            res += [
+                "|",
+                ("public_categ_ids", "in", allowed_categs.ids),
+                ("public_categ_ids", "=", False),
+            ]
+        return res
---
+++ website_sale_restrict_category/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Adds the capability of controlling portal/public users' access to eCommerce products and categories.
