eb873310200e00cdf6efd4f4d36e84c87cd8331f
tslai <tl@quartile.co>
2020-04-24 14:11:36 +0800

    [ADD] stock_view_adj_rpl

---
+++ stock_view_adj_rpl/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock View Adjustments",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "LGPL-3",
+    "depends": ["delivery"],
+    "data": ["views/stock_picking_views.xml"],
+    "installable": True,
+}
