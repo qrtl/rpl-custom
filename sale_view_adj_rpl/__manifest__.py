92fd878db09ebd46eea1617f0394dfafac22b2b2
YogeshMahera-SerpentCS <yogesh.mahera.serpentcs@gmail.com>
2020-05-04 18:59:52 +0530

    [ADD]Added Sale View Adjustment Module

---
+++ sale_view_adj_rpl/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019-2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale View Adj RPL",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["sale_stock"],
+    "data": ["views/sale_order_views.xml"],
+}
