ee5995f1c67a9a68a157ec928b90a189cfa5f3dc
YogeshMahera-SerpentCS <yogesh.mahera.serpentcs@gmail.com>
2020-05-05 20:29:13 +0530

    [FIX]Improved Manifest

--- sale_view_adj_rpl/__manifest__.py
+++ sale_view_adj_rpl/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 Quartile Limited
+# Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Sales Views Adjustments RPL",
