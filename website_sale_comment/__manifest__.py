13208587a00c38b6b07888185371a24678f3dac0
YogeshMahera-SerpentCS <yogesh.mahera.serpentcs@gmail.com>
2020-05-06 20:35:03 +0530

    [IMP]Improved Code

--- website_sale_comment/__manifest__.py
+++ website_sale_comment/__manifest__.py
@@ -2,9 +2,9 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Website Sale Comment",
-    "summary": "Comments texts templates on Sale documents",
+    "summary": "Allows to leave a message when checkouting an order from website",
     "version": "12.0.1.0.0",
-    "category": "Sale",
+    "category": "Website",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "license": "LGPL-3",
