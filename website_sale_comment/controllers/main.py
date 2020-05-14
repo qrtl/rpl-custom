53531f64efbab26a41e94aa3173d6593dabb82bc
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-05-14 13:06:13 +0530

    [ADD][IMP] Fix pylint

--- website_sale_comment/controllers/main.py
+++ website_sale_comment/controllers/main.py
@@ -8,12 +8,17 @@ from odoo.addons.website_sale.controllers.main import WebsiteSaleForm
 
 
 class WebsiteSaleForm(WebsiteSaleForm):
-
-    @http.route('/website_form/shop.sale.order', type='http', auth="public", methods=['POST'], website=True)
+    @http.route(
+        "/website_form/shop.sale.order",
+        type="http",
+        auth="public",
+        methods=["POST"],
+        website=True,
+    )
     def website_form_saleorder(self, **kwargs):
         order = request.website.sale_get_order()
-        if kwargs.get('Give us your feedback') and order:
-            remarks = kwargs.get('Give us your feedback')
-            order.write({'note2': remarks})
-            kwargs.pop('Give us your feedback')
+        if kwargs.get("Give us your feedback") and order:
+            remarks = kwargs.get("Give us your feedback")
+            order.write({"note2": remarks})
+            kwargs.pop("Give us your feedback")
         return super(WebsiteSaleForm, self).website_form_saleorder(**kwargs)
