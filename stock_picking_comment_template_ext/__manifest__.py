bb7f1508bee03f3d59f9e61e8758c92ea282459e
tslai <tl@quartile.co>
2020-05-18 15:02:13 +0800

    [ADD] account_invoice_comment_template_ext

---
+++ stock_picking_comment_template_ext/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Picking Comments Ext",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "LGPL-3",
+    "depends": ["stock_picking_comment_template"],
+    "data": ['views/report_delivery_document.xml', "views/stock_picking_views.xml"],
+    "installable": True,
+}
---
+++ stock_picking_comment_template_ext/models/stock_picking.py
@@ -0,0 +1,10 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    note2 = fields.Html('Staff Memo')
