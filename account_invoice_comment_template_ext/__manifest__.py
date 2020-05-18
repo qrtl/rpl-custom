095dd9cd7118004ad2cd3b794fbff3a3bf75ce0f
tslai <tl@quartile.co>
2020-05-18 15:02:00 +0800

    [ADD] account_invoice_comment_template_ext

---
+++ account_invoice_comment_template_ext/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Account Invoice Comments Ext",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Accounting",
+    "license": "LGPL-3",
+    "depends": ["account_invoice_comment_template"],
+    "data": ['views/report_invoice.xml', "views/account_invoice_views.xml"],
+    "installable": True,
+}
---
+++ account_invoice_comment_template_ext/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Change "Bottom Comments" to "Staff Memo"
