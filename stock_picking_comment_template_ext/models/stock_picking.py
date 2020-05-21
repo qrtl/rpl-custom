0782ceb2ec888358c2cd6df4f5a25d65476619b1
tslai <tl@quartile.co>
2020-05-21 15:06:47 +0800

    [IMP] Make note2 compute field in stock.picking

--- stock_picking_comment_template_ext/models/stock_picking.py
+++ stock_picking_comment_template_ext/models/stock_picking.py
@@ -1,10 +1,18 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    note2 = fields.Html("Staff Memo")
+    note2 = fields.Html(
+        "Staff Memo",
+        compute="_compute_note2",
+    )
+
+    @api.multi
+    def _compute_note2(self):
+        for picking in self:
+            picking.note2 = picking.group_id.sale_id.note2
