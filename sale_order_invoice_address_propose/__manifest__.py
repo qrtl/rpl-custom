95404dc0e63fdd4d60d0b1e374ca85f9f23ae603
tslai <tl@quartile.co>
2020-05-25 10:16:32 +0800

    [FIX] Remove hls module dependency

--- sale_order_invoice_address_propose/__manifest__.py
+++ sale_order_invoice_address_propose/__manifest__.py
@@ -7,7 +7,7 @@
     "website": "https://www.quartile.co",
     "category": "Sales",
     "license": "AGPL-3",
-    "depends": ["sale_stock", "sale_view_adj_hls"],
+    "depends": ["sale_stock"],
     "data": ["views/res_partner_views.xml", "views/sale_order_views.xml"],
     "installable": True,
 }
