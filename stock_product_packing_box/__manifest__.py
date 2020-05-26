928e8bba2f836a8006dcf59c34888611cc22c0b4
Tim <tl@quartile.co>
2020-05-26 16:13:41 +0800

    [464][FIX] Remove UserError in _get_packing_boxes() and ensure_one() in _compute_box_line_ids() (#40)

--- stock_product_packing_box/__manifest__.py
+++ stock_product_packing_box/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Product Packing Box",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "author": "Quartile Limited",
     "category": "Product",
     "website": "https://www.quartile.co",
