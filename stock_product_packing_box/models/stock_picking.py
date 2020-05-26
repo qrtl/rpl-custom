08239d984d0e780ee7839e1a1d52c71a8094ebe6
Tim <tl@quartile.co>
2020-05-26 16:13:41 +0800

    [464][FIX] Remove UserError in _get_packing_boxes() and ensure_one() in _compute_box_line_ids() (#40)

--- stock_product_packing_box/models/stock_picking.py
+++ stock_product_packing_box/models/stock_picking.py
@@ -1,8 +1,7 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
@@ -47,40 +46,40 @@ class StockPicking(models.Model):
         res = self.env["product.packing.box"].search(
             [("is_for_liquid", "=", has_liquid)], order="packing_coefficient asc",
         )
-        if not res:
-            raise UserError(_("No corresponding boxes can be found."))
         return res
 
+    @api.multi
     @api.depends("move_lines.product_uom_qty", "move_lines.reserved_availability")
     def _compute_box_line_ids(self):
-        self.ensure_one()
-        self.box_line_ids.unlink()
-        moves = self._get_moves_for_box_calc()
-        min_box = self._get_min_box(moves)
-        has_liquid = any(move.product_id.has_liquid for move in moves)
-        packing_boxes = self._get_packing_boxes(has_liquid)
-        coefficient_bal = moves._get_packing_coefficient()
-        box_vals = {}
-        # Find the box that has the smallest packing_coefficient that fits
-        # coefficient_bal. Use the largest box if not found, and loop
-        # until remaining coefficient is not larger than 0.
-        while coefficient_bal > 0.0:
-            box = False
-            for packing_box in packing_boxes:
-                if packing_box.packing_coefficient >= coefficient_bal:
-                    box = packing_box
-                    break
-            box = box or packing_boxes[-1]
-            coefficient_bal -= box.packing_coefficient
-            box = box._convert_packing_box(min_box)
-            if box.id not in box_vals:
-                box_vals[box.id] = 1
-            else:
-                box_vals[box.id] += 1
-        self.box_line_ids = [
-            (0, 0, {"packing_box_id": box, "box_quantity": number})
-            for box, number in box_vals.items()
-        ]
+        for picking in self:
+            picking.box_line_ids.unlink()
+            moves = picking._get_moves_for_box_calc()
+            min_box = picking._get_min_box(moves)
+            has_liquid = any(move.product_id.has_liquid for move in moves)
+            packing_boxes = picking._get_packing_boxes(has_liquid)
+            coefficient_bal = moves._get_packing_coefficient()
+            box_vals = {}
+            # Find the box that has the smallest packing_coefficient that fits
+            # coefficient_bal. Use the largest box if not found, and loop
+            # until remaining coefficient is not larger than 0.
+            if packing_boxes:
+                while coefficient_bal > 0.0:
+                    box = False
+                    for packing_box in packing_boxes:
+                        if packing_box.packing_coefficient >= coefficient_bal:
+                            box = packing_box
+                            break
+                    box = box or packing_boxes[-1]
+                    coefficient_bal -= box.packing_coefficient
+                    box = box._convert_packing_box(min_box)
+                    if box.id not in box_vals:
+                        box_vals[box.id] = 1
+                    else:
+                        box_vals[box.id] += 1
+                picking.box_line_ids = [
+                    (0, 0, {"packing_box_id": box, "box_quantity": number})
+                    for box, number in box_vals.items()
+                ]
 
     def recompute_product_packing(self):
         self._compute_box_line_ids()
