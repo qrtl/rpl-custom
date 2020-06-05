5fa44a89a9185ae79a25a265b3abd2489f6bf623
Yoshi Tashiro <tashiro@quartile.co>
2020-06-05 23:10:43 +0800

    [ADD] website_sale_hide_parent

---
+++ website_sale_hide_parent/models/res_partner.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    hide_self_for_children = fields.Boolean(
+        help="If selected, 'Hide Parent' will be selected by default for the "
+             "delivery addresses created through the eCommerce interface."
+    )
