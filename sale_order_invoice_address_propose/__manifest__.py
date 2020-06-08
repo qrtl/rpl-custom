ba8b69749fa97250f8102a28c78bfa45aa7ae335
Tim <tl@quartile.co>
2020-06-08 14:26:58 +0800

    [1036][IMP] Remove domain in partner_shipping_id (#50)

--- sale_order_invoice_address_propose/__manifest__.py
+++ sale_order_invoice_address_propose/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Order Invoice Address Propose",
-    "version": "12.0.1.0.2",
+    "version": "12.0.1.0.3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Sales",
