d27c5d73250da6f79aa2049225a6f096d219842b
tslai <tl@quartile.co>
2020-06-12 10:29:20 +0800

    [FIX] avoid ambiguous naming

--- sale_order_export_rpl/report/report_sale_order_line_csv.py
+++ sale_order_export_rpl/report/report_sale_order_line_csv.py
@@ -13,7 +13,9 @@ class SaleOrderLineCSV(models.AbstractModel):
     def generate_csv_report(self, writer, data, orders):
         writer.writeheader()
         for order in orders:
-            for order_line in order.order_line.filtered(lambda l: not l.is_delivery):
+            for order_line in order.order_line.filtered(
+                lambda line: not line.is_delivery
+            ):
                 writer.writerow(
                     {
                         "Order ID": order.rakushisu_order_id,
