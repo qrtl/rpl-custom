3e7856b0d1e7afb50947f3cb10c6e709c6612374
Tim <tl@quartile.co>
2020-06-17 10:10:27 +0800

    [1038][IMP] Add sales price information to delivery note (#53)

---
+++ stock_picking_report_valued_ext/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Valued Picking Report Ext",
+    "summary": "Adding Valued Picking on Delivery Slip report",
+    "version": "12.0.1.0.0",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "depends": ["stock_picking_comment_template", "stock_picking_report_valued"],
+    "data": ["report/stock_picking_report_valued.xml"],
+    "installable": True,
+}
