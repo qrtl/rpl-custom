2ed36402f185762d330984f78ba9e7c4b7e8ba00
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-18 20:17:01 +0530

    [ADD] Added website_shop_address_adj_rpl module

---
+++ website_shop_address_adj_rpl/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Shop Address Adjustments",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["website_sale"],
+    "data": ["views/website_sale_address_templates.xml"],
+}
---
+++ website_shop_address_adj_rpl/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Removed Billing address from Checkout and Payment page.
