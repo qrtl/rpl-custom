51f1e872569c97f1b146bc55cab76c35a60477ad
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-18 20:17:10 +0530

    [FIX] Fix Pylint,xml

--- website_shop_address_adj_rpl/__manifest__.py
+++ website_shop_address_adj_rpl/__manifest__.py
@@ -2,9 +2,8 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Website Shop Address Adjustments",
-    "summary": "",
     "version": "12.0.1.0.0",
-    "category": "Sales",
+    "category": "Website",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "LGPL-3",
