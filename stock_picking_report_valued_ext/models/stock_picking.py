cfdbd78d09599b904e02903f5ca93cb9b173e9dc
tslai <tl@quartile.co>
2020-06-19 18:25:47 +0800

    [ADD] Back, isort

--- stock_picking_report_valued_ext/models/stock_picking.py
+++ stock_picking_report_valued_ext/models/stock_picking.py
@@ -8,17 +8,21 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     delivery_price = fields.Monetary(
-        string='Delivery Price',
-        compute='_compute_delivery_price',
+        string="Delivery Price",
+        compute="_compute_delivery_price",
         store=True,
         readonly=False,
     )
 
     @api.multi
-    @api.depends('sale_id')
+    @api.depends("sale_id")
     def _compute_delivery_price(self):
         for picking in self:
             if picking.sale_id:
                 picking.delivery_price = sum(
-                    [line.price_subtotal for line in picking.sale_id.order_line if line.is_delivery]
+                    [
+                        line.price_subtotal
+                        for line in picking.sale_id.order_line
+                        if line.is_delivery
+                    ]
                 )
