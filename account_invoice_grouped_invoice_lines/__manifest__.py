f5d48f3e62968149f65465d85f50986b62fe971d
Tim <tl@quartile.co>
2020-06-19 17:22:22 +0800

    Update __manifest__.py

--- account_invoice_grouped_invoice_lines/__manifest__.py
+++ account_invoice_grouped_invoice_lines/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Account Invoice Grouped Lines",
     "version": "12.0.1.0.0",
