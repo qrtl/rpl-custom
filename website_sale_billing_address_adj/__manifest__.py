3cf4dd810020cca9fd5d030429dc888f57f1cd2f
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-23 10:55:59 +0530

    [12.0][T1058] - website_sale_billing_address_adj (#57)
    * [ADD] Added website_shop_address_adj_rpl module
    
    * [FIX] Fix Pylint,xml
    
    * [IMP] Regenerate README and added changes
    
    * [ADD]Added Changes
    
    * [IMP]Improved Code and Fix XPATH, regenrate README, Black
    
    * [IMP/ADD]Fix XML Prettier, Added Translation file
    
    * Update website_sale_billing_address_adj/i18n/ja_JP.po
    
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>
    
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>

---
+++ website_sale_billing_address_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Sale Billing Address Adjustments",
+    "version": "12.0.1.0.0",
+    "category": "Website",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["website_sale"],
+    "data": ["views/website_sale_address_templates.xml"],
+}
---
+++ website_sale_billing_address_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Shows the correct addresses in the eCommerce frontend when the invoice address is different from the customer (standard Odoo assumes that the customer and the invoice address are the same for eCommerce orders).
