dbbd644855afca585b0c284060dcfb436da314bd
tslai <tl@quartile.co>
2020-07-17 18:41:18 +0800

    [IMP] readme and unit-tests

--- stock_picking_report_valued_ext/README.rst
+++ stock_picking_report_valued_ext/README.rst
@@ -22,7 +22,7 @@ Valued Picking Report Ext
 This module does the following:
 
 * Adjusts the content/presentation of delivery slip report, showing the amount for each line, etc.
-* Adds delivery_price to picking which reflects the tax included amount of delivery charge from the sales order.
+* Adds delivery_price to picking which reflects the tax-included amount of delivery charge from the sales order.
 * Hides the discount column if there is no discount amoung the move lines.
 
 **Table of contents**
--- stock_picking_report_valued_ext/tests/test_stock_picking.py
+++ stock_picking_report_valued_ext/tests/test_stock_picking.py
@@ -10,6 +10,12 @@ class TestStockPicking(common.TransactionCase):
         super().setUp()
         delivery_product = self.env.ref("delivery.product_product_delivery_normal")
         test_product = self.env.ref("product.product_product_25")
+        tax = self.env['account.tax'].create({
+            'name': 'TAX 15%',
+            'amount_type': 'percent',
+            'type_tax_use': 'sale',
+            'amount': 15.0,
+        })
         self.sale_order = self.env["sale.order"].create(
             {
                 "partner_id": self.env.ref("base.res_partner_4").id,
@@ -24,7 +30,7 @@ class TestStockPicking(common.TransactionCase):
                             "product_uom": delivery_product.uom_po_id.id,
                             "price_unit": 100,
                             "is_delivery": True,
-                            "tax_id": False,
+                            "tax_id": [(4, tax.id)],
                         },
                     ),
                     (
@@ -37,7 +43,7 @@ class TestStockPicking(common.TransactionCase):
                             "product_uom": test_product.uom_po_id.id,
                             "price_unit": 200,
                             "is_delivery": False,
-                            "tax_id": False,
+                            "tax_id": [(4, tax.id)],
                         },
                     ),
                 ],
@@ -48,10 +54,10 @@ class TestStockPicking(common.TransactionCase):
     def test_01_delivery_price(self):
         """This Method evaluate the delivery price from sale order lines"""
         delivery_price = self.sale_order.picking_ids[0].delivery_price
-        self.assertEqual(delivery_price, 100)
+        self.assertEqual(delivery_price, 115)
 
     def test_02_amount_total(self):
         """This Method evaluate the delivery price should be added in amount total"""
         self.sale_order.picking_ids.action_done()
         amount_total = self.sale_order.picking_ids[0].amount_total
-        self.assertEqual(amount_total, 300)
+        self.assertEqual(amount_total, 345)
