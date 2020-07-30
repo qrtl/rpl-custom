f82cb565af7e786edb50a4363511c8b6989b5344
tslai <tl@quartile.co>
2020-07-30 11:43:49 +0800

    [IMP] Use _get_new_picking_values() to set delivery_price

---
+++ stock_picking_report_valued_ext/models/stock_move.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _get_new_picking_values(self):
+        vals = super()._get_new_picking_values()
+        if self.sale_line_id and self.sale_line_id.order_id:
+            vals["delivery_price"] = sum(
+                [
+                    line.price_reduce_taxinc
+                    for line in self.sale_line_id.order_id.order_line
+                    if line.is_delivery
+                ]
+            )
+        return vals
