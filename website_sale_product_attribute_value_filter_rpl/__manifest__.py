415d8b62767cb7f08efff1fbd9cb9f1e005a9112
Tim <tl@quartile.co>
2020-08-17 21:26:28 +0800

    Update __manifest__.py

--- website_sale_product_attribute_value_filter_rpl/__manifest__.py
+++ website_sale_product_attribute_value_filter_rpl/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Website Sale Attribute Value Filter RPL",
     "summary": "",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Website",
     "website": "https://www.quartile.co",
     "author": "Quartile Limited",
