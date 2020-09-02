c0ae0478e7118e9b503783e7e9ea4778aa90289b
Yoshi Tashiro <tashiro@quartile.co>
2020-09-02 23:00:28 +0800

    [ADD] report_company_address

--- report_common_rpl/models/res_company.py
+++ report_common_rpl/models/res_company.py
@@ -1,5 +1,5 @@
-# Copyright 2019 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2019-2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
 
@@ -7,6 +7,7 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
+    company_name_address = fields.Text("Name & Address", help="Company name and address that should show in report header when set.", translate=True)
     company_chop = fields.Binary("Company Chop Image", attachment=True,)
     fax = fields.Char("Fax")
     ceo = fields.Char("Representative Director")
---
+++ report_company_address/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds common fields that are used in py3o reports.
