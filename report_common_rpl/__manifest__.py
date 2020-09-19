3dcc1c0617d0c5edc2274980fd9d6ed6d4c953c5
Yoshi Tashiro <tashiro@quartile.co>
2020-09-19 12:52:58 +0800

    revert changes on report_common_rpl

--- report_common_rpl/__manifest__.py
+++ report_common_rpl/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright 2019-2020 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Report Common Py3o",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.0.0",
     "category": "Reporting",
     "license": "LGPL-3",
     "author": "Quartile Limited",
-    "depends": ["base"],
+    "depends": ["base", "report_py3o"],
     "data": ["views/res_company_views.xml", "views/res_partner_views.xml"],
     "installable": True,
 }
