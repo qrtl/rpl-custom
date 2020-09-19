6cef828d96ed3ad5a9689e2f520636cad068b8a9
Yoshi Tashiro <tashiro@quartile.co>
2020-09-19 12:54:08 +0800

    revert to agpl

--- report_common_rpl/__manifest__.py
+++ report_common_rpl/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Report Common Py3o",
     "version": "12.0.1.0.0",
     "category": "Reporting",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "author": "Quartile Limited",
     "depends": ["base", "report_py3o"],
     "data": ["views/res_company_views.xml", "views/res_partner_views.xml"],
