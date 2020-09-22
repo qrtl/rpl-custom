f543834b7b2178df737c8d066670502b16ee6ed6
Yoshi Tashiro <tashiro@quartile.co>
2020-09-22 10:58:27 +0900

    [1260][ADD] report_company_address (#84)
    * [ADD] report_company_address

---
+++ report_company_address/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Report Company Address",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "depends": ["web"],
+    "data": ["views/res_company_views.xml", "views/report_templates.xml"],
+    "installable": True,
+}
---
+++ report_company_address/readme/USAGE.rst
@@ -0,0 +1,3 @@
+Update Company Name & Address field newly added in the company form, with
+translation and style adjustments as necessary, and the set name and address
+should show in the report header accordingly.
