653584fcd6323738238e4f99f89dd34c17de0d8d
Ryoko <ryoko@quartile.co>
2020-09-30 15:15:34 +0800

    [ADD] BoM Line tree View

---
+++ bom_line_view_add_rpl/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "BoM Line tree View",
+    "summary": "",
+    "version": "12.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["mrp"],
+    "data": ["views/bom_line_view.xml"],
+}
---
+++ bom_line_view_add_rpl/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* BoM Line tree View.
