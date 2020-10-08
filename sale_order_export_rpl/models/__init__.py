3b49ec6f514d2234aa8ee84e57ff8d24c17d603b
tslai <tl@quartile.co>
2020-10-08 10:16:33 +0800

    [953][DEL] sale_order_export_rpl

--- sale_order_export_rpl/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import product_product
-from . import res_config_settings
-from . import res_partner
-from . import sale_order
--- sale_order_export_rpl/report/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import report_custom_csv
-from . import report_sale_order_csv
-from . import report_sale_order_line_csv
