6129a5e511d9a687b72dd34d128a0ffc6eb51360
tslai <tl@quartile.co>
2020-10-20 18:23:05 +0800

    [ADD] sale_order_place

---
+++ sale_order_place/models/sale_order.py
@@ -0,0 +1,10 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    place = fields.Char('Place')
---
+++ sale_order_place/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module deos the following:
+
+- Add "Place" field to sales order, display in sale order and pro-forma printing.
