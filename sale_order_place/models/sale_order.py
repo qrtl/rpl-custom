913f9f48e42fe88c64e923f8a7f1c949db9056d9
tslai <tl@quartile.co>
2020-10-20 18:23:54 +0800

    [ADD] Black, isort

--- sale_order_place/models/sale_order.py
+++ sale_order_place/models/sale_order.py
@@ -1,10 +1,10 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    place = fields.Char('Place')
+    place = fields.Char("Place")
