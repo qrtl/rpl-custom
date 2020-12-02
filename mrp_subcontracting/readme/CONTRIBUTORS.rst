9dd64385dd0e20d497e6b09ac8e1e4cbd497adf9
Yoshi Tashiro <tashiro@quartile.co>
2020-12-02 14:34:23 +0800

    [REM] mrp_subcontracting, mrp_subcontracting_purchase_link

--- mrp_subcontracting/readme/CONTRIBUTORS.rst
+++
@@ -1,5 +0,0 @@
-* Odoo S.A.
-* `Tecnativa <https://www.tecnativa.com>`__:
-
-  * Alexandre Díaz
-  * Pedro M. Baeza
--- mrp_subcontracting/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-
-from . import mrp_product_produce
-from . import stock_backorder_confirmation
-from . import stock_picking_return
