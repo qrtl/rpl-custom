b9847f9527dc97b23853da5c48b38b4d7777ecef
Yoshi Tashiro <tashiro@quartile.co>
2021-02-04 10:18:56 +0800

    black

--- mrp_production_component_lot_constraint/wizards/change_production_qty.py
+++ mrp_production_component_lot_constraint/wizards/change_production_qty.py
@@ -5,7 +5,7 @@ from odoo import api, models
 
 
 class ChangeProductionQty(models.TransientModel):
-    _inherit = 'change.production.qty'
+    _inherit = "change.production.qty"
 
     @api.multi
     def change_prod_qty(self):
