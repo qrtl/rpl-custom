977cdebb77f2b9b8e13560b45ad456d117c2e604
tslai <tl@quartile.co>
2021-02-16 17:02:20 +0800

    [ADD] Black, isort

--- website_sale_comment/controllers/main.py
+++ website_sale_comment/controllers/main.py
@@ -3,7 +3,7 @@
 
 from odoo import _, http
 from odoo.http import request
-from odoo.tools import plaintext2html, html2plaintext
+from odoo.tools import html2plaintext, plaintext2html
 
 from odoo.addons.website_sale.controllers.main import WebsiteSaleForm
 
@@ -18,18 +18,15 @@ class WebsiteSaleForm(WebsiteSaleForm):
     )
     def website_form_saleorder(self, **kwargs):
         order = request.website.sale_get_order()
-        request.session['Give us your feedback'] = kwargs.get("Give us your feedback")
+        request.session["Give us your feedback"] = kwargs.get("Give us your feedback")
         note_list = []
-        if request.env['sale.order']._default_note():
-            note_list.append(plaintext2html(request.env['sale.order']._default_note()))
+        if request.env["sale.order"]._default_note():
+            note_list.append(plaintext2html(request.env["sale.order"]._default_note()))
         if kwargs.get("increase_refrigerant") and order:
             note_list.append(_("Increase Refrigerant: True"))
         if kwargs.get("Give us your feedback") and order:
             remarks = kwargs.get("Give us your feedback")
             note_list.append(_("Customer Remarks: ") + remarks)
         note = "<br>".join(note_list)
-        order.write({
-            "note": html2plaintext(note),
-            "note2": note
-        })
+        order.write({"note": html2plaintext(note), "note2": note})
         return super(WebsiteSaleForm, self).website_form_saleorder(**kwargs)
