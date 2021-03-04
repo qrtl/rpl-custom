0830ef4420861e1f190ee27f47022a36738c9287
Tim <tl@quartile.co>
2021-03-04 09:52:35 +0800

    st1 [1648][ADD] base_company_reporting (#137)
    * [ADD] base_company_reporting
    
    * [IMP] Adjust report_common_rpl
    
    * [ADD] Black, isort

---
+++ base_company_reporting/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Company Reporting Tab",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "depends": ["base"],
+    "data": ["views/res_company_views.xml"],
+    "installable": True,
+}
---
+++ base_company_reporting/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds the "Reporting" tab to company form view.
