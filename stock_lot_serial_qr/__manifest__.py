b45cf20cd9313ce412d36bff83d8703479ed80e2
tack-yabeQuartile <yabe@quartile.co>
2021-08-17 10:30:35 +0800

    initial_commit_add_stock_lot_serial_qr

---
+++ stock_lot_serial_qr/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Lot Serial QR",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "depends": ["", ""],
+    "data": ["", "views/stock_picking_views.xml"],
+    "installable": True,
+}
