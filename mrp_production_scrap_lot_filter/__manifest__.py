d0c158fd468ed16aee708893350d0d5b82755cb6
Yoshi Tashiro <tashiro@quartile.co>
2021-08-21 16:35:03 +0800

    rename modules, gen readme

--- mrp_production_scrap_lot_filter/__manifest__.py
+++ mrp_production_scrap_lot_filter/__manifest__.py
@@ -1,15 +1,12 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Stock Picking Scrap Filter Lot",
+    "name": "MRP Production Scrap Lot Filter",
     "version": "12.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Manufacturing",
     "license": "LGPL-3",
-    "depends": ["stock"],
-    "data": [
-        "views/stock_scrap_views.xml",
-    ],
+    "depends": ["mrp", "stock_picking_scrap_lot_filter"],
     "installable": True,
 }
--- stock_picking_scrap_lot_filter/__manifest__.py
+++ stock_picking_scrap_lot_filter/__manifest__.py
@@ -1,12 +1,15 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "MRP Production Scrap Filter Lot",
+    "name": "Stock Picking Scrap Lot Filter",
     "version": "12.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Manufacturing",
     "license": "LGPL-3",
-    "depends": ["mrp", "stock_picking_scrap_filter_lot"],
+    "depends": ["stock"],
+    "data": [
+        "views/stock_scrap_views.xml",
+    ],
     "installable": True,
 }
