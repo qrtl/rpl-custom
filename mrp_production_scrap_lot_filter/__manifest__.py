3c6dc18c5421c7eaeabd6a63c868c2cf97a067cf
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2021-08-24 14:10:21 +0900

    st1 [1935][ADD] stock_picking_scrap_lot_filter, mrp_production_scrap_lot_filter (#154)
    * add mrp_production_scrap_filter_lot
    
    * rename modules, gen readme
    
    * pre-commit

---
+++ mrp_production_scrap_lot_filter/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "MRP Production Scrap Lot Filter",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Manufacturing",
+    "license": "LGPL-3",
+    "depends": ["mrp", "stock_picking_scrap_lot_filter"],
+    "installable": True,
+}
