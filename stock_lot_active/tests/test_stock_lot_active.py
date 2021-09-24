f8e28f70c2d79c1664c465a55d6d331f1e5c4c97
Yoshi Tashiro <tashiro@quartile.co>
2021-09-24 21:38:28 +0800

    add test

--- stock_lot_active/tests/test_stock_lot_active.py
+++ stock_lot_active/tests/test_stock_lot_active.py
@@ -1,36 +1,26 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import ValidationError
 from odoo.tests import SavepointCase, tagged
 
 
-@tagged("post_install", "-at_install")
+@tagged("standard", "at_install")
 class TestStockLotActive(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
 
-        cls.lot_obj = cls.env["stock.production.lot"].sudo(cls.user_demo)
         picking_type = cls.env["stock.picking.type"].browse(
             cls.env["ir.model.data"].xmlid_to_res_id("stock.picking_type_in")
         )
         stock_location = cls.env.ref("stock.stock_location_stock")
         supplier_location = cls.env.ref("stock.stock_location_suppliers")
-        product = cls.env["product.product"]
 
-        def _create_product(name, lot_type=False):
-            return product.create(
-                {
-                    "name": name,
-                    "type": "product",
-                    "tracking": "lot",
-                    # "lot_type": lot_type,
-                }
-            )
-
-        cls.prod_sol = _create_product("Solution", "sol")
+        product = cls.env["product.product"].create(
+            {"name": "test product", "type": "product", "tracking": "lot"}
+        )
+        cls.lot = cls.env["stock.production.lot"].create({"product_id": product.id})
 
         cls.picking = cls.env["stock.picking"].create(
             {
@@ -39,28 +29,30 @@ class TestStockLotActive(SavepointCase):
                 "picking_type_id": picking_type.id,
             }
         )
+        cls.move = cls.env["stock.move"].create(
+            {
+                "name": product.name,
+                "product_id": product.id,
+                "product_uom": product.uom_id.id,
+                "product_uom_qty": 1.0,
+                "location_id": supplier_location.id,
+                "location_dest_id": stock_location.id,
+                "picking_id": cls.picking.id,
+            }
+        )
 
-        def _create_move(product):
-            return cls.env["stock.move"].create(
-                {
-                    "name": product.name,
-                    "product_id": product.id,
-                    "product_uom": product.uom_id.id,
-                    "product_uom_qty": 10.0,
-                    "location_id": supplier_location.id,
-                    "location_dest_id": stock_location.id,
-                    "picking_id": cls.picking.id,
-                }
-            )
+    def test_01_archive_lot(self):
+        # There should be no constraint error when there is no associated quants.
+        self.lot.active = False
 
-        cls.move1 = _create_move(cls.prod_placeholder)
+        self.lot.active = True
 
-    def test_01_receive_sol(self):
-        self.move1.product_id = self.prod_sol
         self.picking.action_confirm()
         self.picking.action_assign()
-        lot = self.lot_obj.create({"product_id": self.move1.product_id.id})
-        self.picking.move_line_ids[0].lot_id = lot
+        self.picking.move_line_ids[0].lot_id = self.lot
+        self.picking.move_line_ids[0].qty_done = 1.0
         self.picking.button_validate()
-        lot.date_received = fields.Date.from_string("2020-12-09")
-        self.assertEqual(lot.ref, "VS1209AA")
+        self.assertEqual(len(self.lot.quant_ids), 2)
+        # Error should be raised when there is there is a quant linked to the lot.
+        with self.assertRaises(ValidationError):
+            self.lot.active = False
