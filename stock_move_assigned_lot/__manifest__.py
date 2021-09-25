84a052e7a88d76d981347a658299fee3eaa45a2c
Yoshi Tashiro <tashiro@quartile.co>
2021-09-25 15:14:53 +0800

    translation, readme

---
+++ stock_move_assigned_lot/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock Move Assigned Lot",
+    "version": "12.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Stock",
+    "license": "LGPL-3",
+    "depends": ["stock"],
+    "data": ["views/stock_move_views.xml"],
+    "installable": True,
+}
