5efb15232caefc9944680d2d6ee7209a56b55937
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2021-10-15 19:35:04 +0900

    st1 [2036][IMP] mrp_production_component_lot_constraint: change_prod_qty() (#159)
    [2036](https://www.quartile.co/web?debug=1#id=2036&action=771&model=project.task&view_type=form&menu_id=505)
    
    - [IMP] mrp_production_component_lot_constraint: change_prod_qty()
    
    Before this change, the production quantity change operation would generate rather
    random/inconsistent outputs in terms of the component reservation.
    
    This update should prevent the removal of the already assigned lots, and re-reserve
    the quants for each compnent move according to the new quantities.

--- mrp_production_component_lot_constraint/__manifest__.py
+++ mrp_production_component_lot_constraint/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "MRP Production Component Lot Constraint",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Manufacturing",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
