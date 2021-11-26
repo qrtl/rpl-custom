3235e65da6f81ea88c1acc6ceab31af954c0fa02
tack-yabeQuartile <yabe@quartile.co>
2021-11-26 17:26:23 +0800

    initial commit

---
+++ product_hide_cost/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Product Hide Cost",
+    "summary": "",
+    "version": "12.0.1.0.1",
+    "category": "Contact",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["product", "stock"],
+    "data": ["views/product_product_views.xml"],
+}
---
+++ product_hide_cost/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Adjust list views of product.product to restrict the stock_user from displaying the cos price (standard_price).
