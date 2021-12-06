a9736bf14294d8f9a8066157fe85ed04aececf29
tack-yabeQuartile <yabe@quartile.co>
2021-12-06 15:40:28 +0800

    update translate and pre-commit

--- stock_picking_ready_flag/models/picking_type.py
+++ stock_picking_ready_flag/models/picking_type.py
@@ -1,7 +1,8 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import models
+
 
 class PickingType(models.Model):
     _inherit = "stock.picking.type"
--- stock_picking_ready_flag/models/stock_picking.py
+++ stock_picking_ready_flag/models/stock_picking.py
@@ -7,4 +7,8 @@ from odoo import fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    is_ready = fields.Boolean(default=False, string="Ready", help="Please select this when the delivery is ready to be processed.")
+    is_ready = fields.Boolean(
+        default=False,
+        string="Ready",
+        help="Please select this when the delivery is ready to be processed.",
+    )
