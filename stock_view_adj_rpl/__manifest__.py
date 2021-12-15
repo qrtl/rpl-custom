1839677aa6c3a1b0632d8565495ceeb30ac97d0e
tack-yabeQuartile <yabe@quartile.co>
2021-12-16 05:14:41 +0800

    add a xml, update manifest readme, pre-commit

--- stock_view_adj_rpl/__manifest__.py
+++ stock_view_adj_rpl/__manifest__.py
@@ -2,12 +2,12 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Stock View Adjustments",
-    "version": "12.0.1.0.2",
+    "version": "12.0.1.0.3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Stock",
     "license": "LGPL-3",
-    "depends": ["delivery"],
-    "data": ["views/stock_picking_views.xml"],
+    "depends": ["delivery", "stock_move_location"],
+    "data": ["views/stock_picking_views.xml", "views/stock_move_location_views.xml"],
     "installable": True,
 }
