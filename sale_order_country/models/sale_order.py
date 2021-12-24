1aa221bbed8bd03bb892cad148c6842859943e6e
Tack Yabe <85266439+tack-yabeQuartile@users.noreply.github.com>
2021-12-24 17:04:31 +0900

    st1 [2188][ADD] sale_order_country (#181)
    * add sale_order_display_country
    
    * rename_module and adjust few expressions and fix some files.
    
    * pre-commit, fix translation

---
+++ sale_order_country/models/sale_order.py
@@ -0,0 +1,11 @@
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    country_id = fields.Many2one(related="partner_id.country_id", store=True,)
+    country_code = fields.Char(related="partner_id.country_id.code", store=True,)
