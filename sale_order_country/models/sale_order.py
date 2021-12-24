448c14918d3dc34ca650c7f6b1b9f4493ca65496
tack-yabeQuartile <yabe@quartile.co>
2021-12-24 15:16:52 +0800

    pre-commit

--- sale_order_country/models/sale_order.py
+++ sale_order_country/models/sale_order.py
@@ -7,7 +7,5 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    country_id = fields.Many2one(
-        related="partner_id.country_id", store=True,
-    )
+    country_id = fields.Many2one(related="partner_id.country_id", store=True,)
     country_code = fields.Char(related="partner_id.country_id.code", store=True,)
