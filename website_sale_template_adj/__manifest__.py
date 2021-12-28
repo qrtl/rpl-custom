e84d2ada99e85e95300f06bd7ea4b0814e788111
kakurai8 <80655511+kakurai8@users.noreply.github.com>
2021-12-28 18:19:15 +0900

    st1 [1790][ADD] website_sale_template_adj (#179)
    [1790](https://www.quartile.co/web?debug=1#id=1790&action=771&model=project.task&view_type=form&menu_id=505)
    
    - Ref. odoo>addons>website_sale>views>templates.xml

---
+++ website_sale_template_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "eCommerce: View Adjustments",
+    "version": "12.0.1.0.0",
+    "category": "Website",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["website_sale"],
+    "data": ["views/website_sale_template_adj.xml"],
+}
