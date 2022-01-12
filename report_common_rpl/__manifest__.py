bebf13f733243512a42d096c293fe349aa76037f
Yoshi Tashiro <tashiro@quartile.co>
2022-01-12 12:55:32 +0800

    correct version

--- report_common_rpl/__manifest__.py
+++ report_common_rpl/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Report Common",
-    "version": "12.0.1.2.0",
+    "version": "12.0.1.1.0",
     "category": "Reporting",
     "license": "AGPL-3",
     "author": "Quartile Limited",
