d55e125d5aca1bfb455dda22b1d7093fa8b046df
Yoshi Tashiro <tashiro@quartile.co>
2022-01-12 10:12:27 +0800

    translation

--- report_common_rpl/__manifest__.py
+++ report_common_rpl/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2019-2022 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Report Common Py3o",
-    "version": "12.0.1.1.0",
+    "name": "Report Common",
+    "version": "12.0.1.2.0",
     "category": "Reporting",
     "license": "AGPL-3",
     "author": "Quartile Limited",
-    "depends": ["base_company_reporting", "report_py3o"],
+    "depends": ["base_company_reporting"],
     "data": ["views/res_company_views.xml", "views/res_partner_views.xml"],
     "installable": True,
 }
