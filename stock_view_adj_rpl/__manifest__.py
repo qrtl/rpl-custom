e6e5538f7082d7194fb9c51017afbdfa471ce51b
tack-yabeQuartile <yabe@quartile.co>
2022-01-24 14:10:55 +0800

    create stock_production_lot_views.xml

--- stock_view_adj_rpl/__manifest__.py
+++ stock_view_adj_rpl/__manifest__.py
@@ -1,13 +1,17 @@
-# Copyright 2021 Quartile Limited
+# Copyright 2021-2022 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock View Adjustments",
-    "version": "12.0.1.0.3",
+    "version": "12.0.1.0.4",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Stock",
     "license": "AGPL-3",
     "depends": ["delivery", "stock_move_location"],
-    "data": ["views/stock_picking_views.xml", "wizard/stock_move_location.xml"],
+    "data": [
+        "views/stock_picking_views.xml",
+        "views/stock_production_lot_view.xml",
+        "wizard/stock_move_location.xml"
+    ],
     "installable": True,
 }
