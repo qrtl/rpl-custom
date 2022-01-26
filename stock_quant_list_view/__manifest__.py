4dac1b8f9cad2bd866489b3fe9226a4156832b61
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-01-26 19:00:28 +0900

    [2268][IMP] stock_quant_list_view: inherit standard tree (#197)

--- stock_quant_list_view/__manifest__.py
+++ stock_quant_list_view/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2019 Quartile Limited
+# Copyright 2019-2022 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Quant List View",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Stock",
     "license": "AGPL-3",
-    "depends": ["stock"],
+    "depends": ["stock_available_unreserved"],  # OCA
     "data": ["views/stock_quant_views.xml"],
     "installable": True,
 }
