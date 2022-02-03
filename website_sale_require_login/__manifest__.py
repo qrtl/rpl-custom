770d94b9cb6a984c4b0b52268c3d2e2ff8d5c41d
tack-yabeQuartile <yabe@quartile.co>
2022-02-03 09:52:00 +0800

    update period of copyrights

--- website_sale_require_login/__manifest__.py
+++ website_sale_require_login/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Quartile Limited
+# Copyright 2021-2022 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Website Sale Require Login",
